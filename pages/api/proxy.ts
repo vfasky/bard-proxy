446a418c9f8ea5a8bcd3746897690f0210fa1a7a
vfasky <vfasky@me.com>
2023-12-20 14:22:08 +0800

    feat 支持流程请求

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -11,52 +11,14 @@ export const config = {
   ],
 };
 
-const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
-  const picked = new Headers();
-  for (const key of headers.keys()) {
-    if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
-      const value = headers.get(key);
-      if (typeof value === "string") {
-        picked.set(key, value);
-      }
-    }
-  }
-  return picked;
-};
+const PROXY_HOST = "https://generativelanguage.googleapis.com";
 
-const CORS_HEADERS: Record<string, string> = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "Content-Type, Authorization",
-};
 
 export default async function handleRequest(req: Request & { nextUrl?: URL }) {
-  if (req.method === "OPTIONS") {
-    return new Response(null, {
-      headers: CORS_HEADERS,
-    });
-  }
 
-  const { pathname, searchParams } = req.nextUrl ? req.nextUrl : new URL(req.url);
-  const url = new URL(pathname + `?key=${searchParams.get('key')}`, "https://generativelanguage.googleapis.com").href;
-  // console.log('Debug console url:', url)
-  const headers = pickHeaders(req.headers, ["content-type", "authorization"]);
+  const url = req.nextUrl || new URL(req.url);
 
-  const res = await fetch(url, {
-    body: req.body,
-    method: req.method,
-    headers,
-  });
+  url.host = PROXY_HOST;
+  return await fetch(url, request)
 
-  const resHeaders = {
-    ...CORS_HEADERS,
-    ...Object.fromEntries(
-      pickHeaders(res.headers, ["content-type", /^x-ratelimit-/])
-    ),
-  };
-
-  return new Response(res.body, {
-    headers: resHeaders,
-    status: res.status
-  });
 }
