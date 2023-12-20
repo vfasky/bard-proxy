c297abad5e5fa4476e0b03677f14aad71fc6bde6
vfasky <vfasky@me.com>
2023-12-20 14:33:26 +0800

    fix ci

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
@@ -16,4 +17,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+export default nextConfig
